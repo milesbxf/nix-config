09191b731979a9654a3e1a099abcd857c94de4c9
milesbryant <miles@milesbxf.net>
2022-07-05 10:30:46 +0100

    Use Kustomize 3.8

--- overlays/kustomize-3-8-9/default.nix
+++ overlays/kustomize-3-8-9/default.nix
@@ -1,9 +1,9 @@
 self: super: {
   kustomize_3 = super.buildGoModule rec {
     pname = "kustomize_3";
-    version = "3.8.9";
+    version = "3.8.0";
     # rev is the commit of the tag, mainly for kustomize version command output
-    rev = "f30fea4c072c794e9b936f20bd1592d9fa25d976";
+    rev = "6a50372dd5686df22750b0c729adaf369fbf193c";
 
     ldflags = let t = "sigs.k8s.io/kustomize/api/provenance"; in [
       "-s -w"
@@ -15,7 +15,7 @@ self: super: {
       owner = "kubernetes-sigs";
       repo = "kustomize";
       rev = "kustomize/v${version}";
-      sha256 = "sha256-kOEaD52qJUHuKlLvucJJuNbYRjZz/jQmL0T78l5GFhU=";
+      sha256 = "sha256-PmZdLamV+CjIVyEvPZ5lmTIw13rSKCfiQwJ2Eyx+Bu0=";
     };
 
     doCheck = true;
@@ -23,7 +23,7 @@ self: super: {
     # avoid finding test and development commands
     sourceRoot = "source/kustomize";
 
-    vendorSha256 = "sha256-QoOsHK6k6DRtMcVDltnRcK1i/tbEDTRA6NKfR+jOkjI=";
+    vendorSha256 = "sha256-vO0kXvojC0b3pEEVo7pvND8UqxvqntIYEEFmm+ID5A8=";
 
     meta = with super.lib; {
       description = "Customization of kubernetes YAML configurations";
