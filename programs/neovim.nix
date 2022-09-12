8d7786cc0ff970f794b306259aac6983c2cb6c47
milesbryant <miles@milesbxf.net>
2022-09-12 11:25:12 +0100

    Fix neovim theme

--- programs/neovim.nix
+++ programs/neovim.nix
@@ -10,16 +10,7 @@ let userlib = import ../userlib {}; in
     withNodeJs = true;
     withPython3 = true;
     extraConfig = ''
-      " Gruvbox theme config; see https://github.com/morhetz/gruvbox/wiki/Configuration
-      let g:gruvbox_italic         = 1
-      let g:gruvbox_bold           = 1
-      let g:gruvbox_underline      = 1
-      let g:gruvbox_undercurl      = 1
-      let g:gruvbox_contrast_light = 'medium'
-      let g:airline_theme          = 'gruvbox'
-
-      colorscheme gruvbox
-      set background=dark
+    colorscheme desert
     '' + (userlib.concatFiles (userlib.lsFiles ./configs/neovim));
 
     plugins = with pkgs.vimPlugins; [
